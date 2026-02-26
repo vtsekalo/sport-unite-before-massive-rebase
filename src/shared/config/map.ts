8476465324033504422182da2403f3c09b37ced4
macromicro37@gmail.com <macromicro37@gmail.com>
2026-02-26 07:50:45 +0000

    Fix map key

--- src/shared/config/map.ts
+++ src/shared/config/map.ts
@@ -2,7 +2,7 @@
  * Константы для работы с картой 2GIS
  */
 
-export const MAP_API_KEY = '4f2a8681-9eb5-4652-af0e-7c11db1e3189';
+export const MAP_API_KEY = import.meta.env.VITE_2GIS_MAP_API_KEY;
 export const DEFAULT_MAP_CENTER: [number, number] = [37.620001, 55.754167];
 export const DEFAULT_MAP_ZOOM = 14;
 
